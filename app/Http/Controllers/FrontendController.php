ce1ce20f5ff8df2937eb922ba123684daea04fa9
Adolfo Martinez <adolfocod00@gmail.com>
2023-03-15 20:41:09 +0100

    Cambios webrequest

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -26,6 +26,7 @@ class FrontendController extends Controller{
         return view('web.static.aboutus');
     }
 
+    #Seminarios
     public function allSeminar(){
         $seminarList = Seminar::orderBy('year','desc')->get();
         return view('web.seminar.all', ['seminarList'=>$seminarList]);
@@ -42,6 +43,7 @@ class FrontendController extends Controller{
         return view('web.seminar.show', $data);
     }
 
+    #Presentaciones
     public function allPresentation(){
         $presentationList = Presentation::all();
         $yearList = Seminar::orderBy('year','desc')->select('year', 'location')->get();
@@ -61,10 +63,58 @@ class FrontendController extends Controller{
 
         return view('web.presentation.show', $data);
     }
+
+    #peticiones
     public function allRequest(){
         $requestList = Myrequest::where("user_id",auth()->user()->id)->get()??null;
         $data['requestList']=$requestList;
         return view ('web.request.all', $data);
+    }
+
+    public function newRequest(){
+        $u = User::all();
+        $p = Presentation::all();
+        $s = Seminar::all();
+        $data["userList"]= $u;
+        $data["presentationList"]= $p;
+        $data["seminarList"]= $s;
+        $data['myrequest']= null;
+
+        return view('web.request.form', $data);
+    }
+
+    public function editRequest(){
+        $myrequest = Myrequest::find($id);
+        $u = User::all();
+        $p = Presentation::all();
+        $s = Seminar::all();
+        $data["userList"]= $u;
+        $data["presentationList"]= $p;
+        $data["seminarList"]= $s;
+        $data['myrequest']= $myrequest;
+
+        return view('web.request.form', $data);
+    }
+
+    public function deleteRequest(){
+        
+    }
+
+    public function storeRequest(Request $r) {
+        $myrequest = new Myrequest($r->all());
+        $myrequest->document_id = $this->storeDocument($r);
+        $myrequest->save();
+ 
+        return redirect()->route('request.index');
+    }
+
+    public function updateRequest(Request $r) {
+        $a = Myrequest::find($id);
+        $this->updateDocument($r, $a->document_id);
+        $a->fill($r->all()); 
+        $a->save(); 
+
+        return redirect()->route('web.request.all');
     }
 
 }

aaaff76f327f3a0936e8f08a33c2666b1675f708
adolfo007123 <adolfocod00@gmail.com>
2023-03-16 13:30:27 +0100

    Rutas borradas get:mydestroy (no se usan)

--- routes/web.php
+++ routes/web.php
@@ -41,23 +41,13 @@ Route::get('/admin',function(){
     return redirect('/admin/seminar');
 });
 Route::resource('admin/seminar', 'SeminarController');
-Route::get('admin/seminar/delete/{seminar}', 'SeminarController@destroy')->name('seminar.myDestroy');
-
 Route::resource('admin/documents', 'DocumentsController');
-Route::get('admin/documents/delete/{documents}', 'DocumentsController@destroy')->name('documents.myDestroy');
-
 Route::resource('admin/speaker', 'SpeakerController');
-Route::get('admin/speaker/delete/{speaker}', 'SpeakerController@destroy')->name('speaker.myDestroy');
-
 Route::resource('admin/presentation', 'PresentationController');
-Route::get('admin/presentation/delete/{presentation}', 'PresentationController@destroy')->name('presentation.myDestroy');
-
 Route::resource('admin/request', 'MyrequestController');
-Route::get('admin/request/delete/{request}', 'MyrequestController@destroy')->name('request.myDestroy');
 Route::get('admin/request/acept/{request}', 'MyrequestController@acept')->name('request.acept');
 
 Route::resource('admin/user', 'UserController');
-Route::get('admin/user/delete/{user}', 'UserController@destroy')->name('user.myDestroy');
 
 /*FIN RUTAS PANEL ADMINISTRADOR*/
 
